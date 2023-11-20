5f192aa66a2a2df100ca07ecbe7a82d8bde80694
mendelsshop <mendelsshop@gmail.com>
2023-11-20 03:32:14 -0500

    trying to use functor -> applicative -> monad hierchy part 1

--- lib/parser.ml
+++ lib/parser.ml
@@ -1,37 +1,24 @@
-type 'a parser = string -> ('a * string, string) result
+module type Functor = sig
+  type 'a f
 
-let first_char string =
-  match string.[0] with char -> Some char | exception _ -> None
+  val fmap : ('a -> 'b) -> 'a f -> 'b f
+end
 
-let rest_string (input : string) =
-  try String.sub input 1 (String.length input - 1)
-  with Invalid_argument _ -> ""
+module Functor (Core : Functor) = struct
+  let ( <$ ) a b = Core.fmap b a
+end
 
-let result v input = Ok (v, input)
-let zero _ = Error ""
+module Option_functor : Functor = struct
+  type 'a f = 'a option
 
-let item input =
-  Option.fold (first_char input)
-    ~some:(fun first -> Ok (first, rest_string input))
-    ~none:(Error input)
-
-let bind p f input = Result.bind (p input) (fun (res, input) -> f res input)
-
-let seq p1 p2 =
-  bind p1 (fun first -> bind p2 (fun second -> result (first, second)))
-
-(* let plus p q input  *)
-let sat p = bind item (fun x -> if p x then result x else zero)
-let char x = sat (fun y -> x == y)
-let digit = sat (fun x -> '0' <= x && x >= '9')
-let lower = sat (fun x -> 'a' <= x && x >= 'z')
-let upper = sat (fun x -> 'A' <= x && x >= 'Z')
+  let fmap f x = match x with Some x -> Some (f x) | None -> None
+end
 
 module type Monad = sig
   type 'a m
 
   val result : 'a -> 'a m
-  val bind : 'a m -> ('a -> 'b m) -> 'b m
+  val ( >>= ) : 'a m -> ('a -> 'b m) -> 'b m
 end
 
 module type MonadOPlus = sig
@@ -41,19 +28,32 @@ module type MonadOPlus = sig
   val ( ++ ) : 'a m -> 'a m -> 'a m
 end
 
-module Parser : Monad with type 'a m = string -> ('a * string) list = struct
-  type 'a m = string -> ('a * string) list
+module Parser : Monad with type 'a m = char list -> ('a * char list) option =
+struct
+  type 'a m = char list -> ('a * char list) option
 
-  let result v input = [ (v, input) ]
-
-  let bind p f input =
-    List.map (fun (v, inp) -> f v inp) (p input) |> List.concat
+  let result v input = Some (v, input)
+  let bind p f input = Option.bind (p input) (fun (v, inp) -> f v inp)
+  let ( >>= ) p q input = Option.bind (p input) (fun (v, inp) -> q v inp)
 end
 
-module Parser : MonadOPlus with type 'a m = string -> ('a * string) list =
-struct
+module Parser :
+  MonadOPlus with type 'a m = char list -> ('a * char list) option = struct
   include Parser
 
-  let zero _ = []
-  let ( ++ ) p q inp = p inp @ q inp
+  let zero _ = None
+  let ( ++ ) p q input =
+    Option.fold ~some:(fun x -> Some x) ~none:(q input) (p input)
 end
+
+open Parser
+
+let item input = match input with [] -> None | s :: rest -> Some (s, rest)
+let sat p = item >>= fun x -> if p x then result x else zero
+let char x = sat (fun y -> x == y)
+let digit = sat (fun x -> '0' <= x && x >= '9')
+let lower = sat (fun x -> 'a' <= x && x >= 'z')
+let upper = sat (fun x -> 'A' <= x && x >= 'Z')
+
+(* let () = *)
+(* let p = upper >>= Parser.lower in *)
