ca385e7792ef282bf3e7ac72e136dbf2d76a27fa
mendelsshop <mendelsshop@gmail.com>
2023-11-23 23:57:04 -0500

    added string unit test

--- test/Parser.ml
+++ test/Parser.ml
@@ -1,16 +1,25 @@
-(* open Parser *)
-
 let test_char x () =
-  Alcotest.(check  (option (pair char (list char))) )
+  Alcotest.(check (option (pair char (list char))))
     ("char " ^ String.make 1 x)
     (Some (x, []))
     ((Parser.char x) (x :: []))
 
-let () = 
-    let open Alcotest in 
-    run "Parsers" [
-        "char", [
-            test_case "x" `Quick (test_char 'x');
-            test_case "y" `Quick (test_char 'y');
-        ]
+let explode string = List.of_seq (String.to_seq string)
+
+let test_string x () =
+  Alcotest.(check (option (pair string (list char))))
+    ("string " ^ x)
+    (Some (x, []))
+    ((Parser.string x) (explode x))
+
+let () =
+  let open Alcotest in
+  run "Parsers"
+    [
+      ( "char",
+        [
+          test_case "x" `Quick (test_char 'x');
+          test_case "y" `Quick (test_char 'y');
+        ] );
+      ("string", [ test_case "hi" `Quick (test_string "hi") ]);
     ]
