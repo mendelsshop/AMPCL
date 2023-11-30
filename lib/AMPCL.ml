50193197e8b97db93b915b9c1ce5f892cccb0c40
mendelsshop <mendelsshop@gmail.com>
2023-11-29 22:41:19 -0500

    Added many sequncing parers combinators along with choice

--- lib/AMPCL.ml
+++ lib/AMPCL.ml
@@ -2,22 +2,34 @@ type ('s, 'a) parser = 's list -> ('a * 's list) option
 
 let ( >> ) f g x = g (f x)
 let explode str = str |> String.to_seq |> List.of_seq
-let result v input = Some (v, input)
-let bind f p input = Option.bind (p input) (fun (v, inp) -> f v inp)
+let return v input = Some (v, input)
 let ( >>= ) p q input = Option.bind (p input) (fun (v, inp) -> q v inp)
+let bind f p input = Option.bind (p input) (fun (v, inp) -> f v inp)
 let zero _ = None
-let map f = bind (f >> result)
+let map f = bind (f >> return)
 let ( <$> ) p f = map f p
 
 let seq p q =
   p >>= fun x ->
-  q >>= fun y -> result (x, y)
+  q >>= fun y -> return (x, y)
+
+let ( << ) p q = p >>= fun _ -> q
+let keep_right = ( << )
+
+let ( >> ) p q =
+  p >>= fun r ->
+  q >>= fun _ -> return r
+
+let keep_left = ( >> )
 
 let ( <|> ) p q input =
   Option.fold ~some:(fun x -> Some x) ~none:(q input) (p input)
 
+let alt = ( <|> )
+let between l r p = l << p >> r
+let rec choice = function [] -> zero | fst :: rest -> fst <|> choice rest
 let item input = match input with [] -> None | s :: rest -> Some (s, rest)
-let sat p = item >>= fun x -> if p x then result x else zero
+let sat p = item >>= fun x -> if p x then return x else zero
 let char x = sat (fun y -> x == y)
 let digit = sat (fun x -> '0' <= x && x <= '9')
 let lower = sat (fun x -> 'a' <= x && x <= 'z')
@@ -28,17 +40,17 @@ let alphanum = letter <|> digit
 let rec word input =
   let neWord =
     letter >>= fun x ->
-    word >>= fun xs -> result (String.make 1 x ^ xs)
+    word >>= fun xs -> return (String.make 1 x ^ xs)
   in
-  (neWord <|> result "") input
+  (neWord <|> return "") input
 
 let string str =
   let rec string_i x =
     match x with
-    | [] -> result ""
+    | [] -> return ""
     | x :: xs ->
         char x >>= fun _ ->
-        string_i xs >>= fun xs -> result (String.make 1 x ^ xs)
+        string_i xs >>= fun xs -> return (String.make 1 x ^ xs)
   in
   let exp_str : char list = List.of_seq (String.to_seq str) in
   string_i exp_str
@@ -46,17 +58,17 @@ let string str =
 let rec many parser =
   let neMany =
     parser >>= fun x ->
-    many parser >>= fun xs -> result (x :: xs)
+    many parser >>= fun xs -> return (x :: xs)
   in
-  neMany <|> result []
+  neMany <|> return []
 
 let many1 p =
   p >>= fun x ->
-  many p >>= fun xs -> result (x :: xs)
+  many p >>= fun xs -> return (x :: xs)
 
 let sepby1 p sep =
   p >>= fun x ->
-  many (seq sep p >>= fun (_, x) -> result x) >>= fun xs -> result (x :: xs)
+  many (seq sep p >>= fun (_, x) -> return x) >>= fun xs -> return (x :: xs)
 
-let sepby p sep = sepby1 p sep <|> result []
-let opt p = ( <$> ) p (fun x -> Some x)  <|> result None
+let sepby p sep = sepby1 p sep <|> return []
+let opt p = p <$> (fun x -> Some x) <|> return None
--- lib/AMPCL.mli
+++ lib/AMPCL.mli
@@ -1,14 +1,25 @@
 type ('s, 'a) parser = 's list -> ('a * 's list) option
 
 val explode : string -> char list
-val result : 'a -> ('s, 'a) parser
+val return : 'a -> ('s, 'a) parser
 val zero : ('s, 'a) parser
 val item : ('a, 'a) parser
 val bind : ('a -> ('s, 'b) parser) -> ('s, 'a) parser -> ('s, 'b) parser
 val ( >>= ) : ('s, 'a) parser -> ('a -> ('s, 'b) parser) -> ('s, 'b) parser
 val ( <|> ) : ('s, 'a) parser -> ('s, 'a) parser -> ('s, 'a) parser
+val alt : ('s, 'a) parser -> ('s, 'a) parser -> ('s, 'a) parser
+val choice : ('s, 'a) parser list -> ('s, 'a) parser
 val map : ('a -> 'b) -> ('s, 'a) parser -> ('s, 'b) parser
+val ( <$> ) : ('s, 'a) parser -> ('a -> 'b) -> ('s, 'b) parser
 val seq : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'a * 'b) parser
+val ( << ) : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'b) parser
+val keep_right : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'b) parser
+val ( >> ) : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'a) parser
+val keep_left : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'a) parser
+
+val between :
+  ('s, 'l) parser -> ('s, 'r) parser -> ('s, 'r) parser -> ('s, 'r) parser
+
 val sat : ('a -> bool) -> ('a, 'a) parser
 val char : char -> (char, char) parser
 val digit : (char, char) parser
