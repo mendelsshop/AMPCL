9a5646eabf813b9413bdb25f8ba3b59cc4cd2252
mendelsshop <mendelsshop@gmail.com>
2024-10-13 22:25:14 -0400

    Playing around with different parsers

--- lib/AMPCL.ml
+++ lib/AMPCL.ml
@@ -1,8 +1,9 @@
 (*TODO: have expected part*)
 type 'e error = { default : string; custom : 'e option }
 
-type ('s, 'a, 'e) parser =
-  's list -> 'e error -> ('a * 's list, 'e error) result
+(*We have to error generics, to make map_error work properly*)
+type ('s, 'a, 'e, 'ee) parser =
+  's list -> 'e error -> ('a * 's list, 'ee error) result
 
 let ( & ) f g x = g (f x)
 let ( &. ) f g x y = g (f x y)
@@ -11,24 +12,16 @@ let ( &. ) f g x y = g (f x y)
 let explode str = str |> String.to_seq |> List.of_seq
 let implode cs = cs |> List.to_seq |> String.of_seq
 
-let run (p : ('s, 'a, 'e) parser) str =
+let run p str =
   (fun (parsed, str') ->
     if List.length str' = 0 then Ok parsed
     else Error { default = "eof"; custom = None })
   |> Result.bind (p (str |> explode) { custom = None; default = "" })
 
-let (return : 'a -> ('s, 'a, 'e) parser) = fun v input _ -> Ok (v, input)
-
-let (( >>= ) :
-      ('s, 'a, 'e) parser -> ('a -> ('s, 'b, 'e) parser) -> ('s, 'b, 'e) parser)
-    =
- fun p q input e -> Result.bind (p input e) (fun (v, inp) -> q v inp e)
-
+let return v input _ = Ok (v, input)
+let ( >>= ) p q input e = Result.bind (p input e) (fun (v, inp) -> q v inp e)
 let bind f p = p >>= f
-
-let zero : ('s, 'a, 'e) parser =
- fun _ _ -> Error { default = "fail"; custom = None }
-
+let zero _ _ = Error { default = "fail"; custom = None }
 let map f = bind (f & return)
 let ( <$> ) p f = map f p
 
@@ -44,33 +37,28 @@ let ( >> ) p q =
   q >>= fun _ -> return r
 
 let keep_left = ( >> )
-
-(*problem is that parser is not a binary function (-> ->), but & is for -> unary*)
-(*let (map_error :*)
-(*      ('e error -> 'ee error) -> ('s, 'a, 'e) parser -> ('s, 'a, 'ee) parser) =*)
-(* fun (f : 'e error -> 'ee error) (p : ('s, 'a, 'e) parser) ->*)
-(*  p &. Result.map_error f*)
-
 (*This does not work because, it takes a parser of 'e and returns a parser of 'ee*)
-(*But, we also take a new error ('ee) for the return parse (along with the input) which is fed into the orginal parser which expects 'e*)
+(*But, we also take a new error ('ee) for the return parse (along with the input) which is fed into the orginal parser which expects 'e (this doesn't work for  base combinators, see item)*)
 (*Maybe just refactor parser definiton*)
-let (map_error :
-      (string error -> int error) -> ('s, 'a, string) parser -> ('s, 'a, int) parser) =
- fun (f : string error -> int error) p i e ->
-   p i (f e)
+let map_error (f : 'e error -> 'ee error) p = p &. Result.map_error f
 
-let internal_label e =
+let internal_label
+    (*:*)
+    (*    (string -> string) ->*)
+    (*    ('s, 'a, 'e, 'ee) parser ->*)
+    (*    ('s, 'a, 'ee, 'ee) parser*) e =
   map_error (fun { custom; default } -> { custom; default = e default })
 
-let (label : 'ee -> ('s, 'a, 'e) parser -> ('s, 'a, 'ee) parser) =
- fun (e : 'ee) ->
+let label (e : 'ee) =
   map_error (fun { custom = _; default } -> { custom = Some e; default })
 
-let ( <|> ) p q input es =
+let ( <|> ) (p : ('s, 'a, 'e, 'ee) parser) (q : ('s, 'a, 'e, 'ee) parser)
+    input es  =
   Result.fold
     ~ok:(fun x -> Ok x)
     ~error:(fun { default = e'; _ } ->
-      (q |> internal_label (fun e -> e' ^ " or " ^ e)) input es)
+      let x = q |> internal_label (fun e -> e' ^ " or " ^ e) in
+      x input es)
     (p input es)
 
 let alt = ( <|> )
@@ -79,27 +67,28 @@ let between l r p = l << p >> r
 (*TODO: make error not contain fail*)
 let rec choice = function [] -> zero | fst :: rest -> fst <|> choice rest
 
-let (item : ('a, 'a, 'e) parser) =
+(*We cannot make this, be (..., 'e, 'e) parser, because then you cannot map error over it, but since we are plugging in the input error in*)
+let (item : ('a, 'a, 'e, 'ee) parser) =
  fun input error ->
   match input with [] -> Error error | s :: rest -> Ok (s, rest)
 
-let sat (p : 'a -> bool) : ('a, 'a, 'e) parser =
+let sat (p : 'a -> bool) : ('a, 'a, 'e, 'ee) parser =
   item >>= fun x -> if p x then return x else zero
 
 let char x = sat (fun y -> x == y) |> internal_label (fun _ -> String.make 1 x)
 
-let digit =
+let digit : (char, char, 'e, 'e) parser =
   sat (fun x -> '0' <= x && x <= '9') |> internal_label (fun _ -> "digit")
 
-let lower =
+let lower : (char, char, 'e, 'e) parser =
   sat (fun x -> 'a' <= x && x <= 'z')
   |> internal_label (fun _ -> "lower case letter")
 
-let upper : ('s, char, 'e) parser =
+let upper : (char, char, 'e, 'e) parser =
   sat (fun x -> 'A' <= x && x <= 'Z')
   |> internal_label (fun _ -> "upper case letter")
 
-let letter : ('s, char, 'e) parser =
+let letter : (char, char, 'e, 'e) parser =
   upper <|> lower |> internal_label (fun _ -> "letter")
 
 let alphanum = letter <|> digit |> internal_label (fun _ -> " digit")
--- lib/AMPCL.mli
+++ lib/AMPCL.mli
@@ -1,63 +1,96 @@
 type 'e error = { default : string; custom : 'e option }
-type ('s, 'a, 'e) parser = 's list -> 'e error -> ('a * 's list, 'e error) result
+
+type ('s, 'a, 'e, 'ee) parser =
+  's list -> 'e error -> ('a * 's list, 'ee error) result
 
 val explode : string -> char list
 val implode : char list -> string
-val run : (char, 'a, 'e) parser -> string -> ('a, 'e error) result
+val run : (char, 'a, 'e, 'ee) parser -> string -> ('a, 'ee error) result
+
 val ( >>= ) :
-  ('s, 'a, 'e) parser -> ('a -> ('s, 'b, 'e) parser) -> ('s, 'b, 'e) parser
-val return : 'a -> ('s, 'a, 'e) parser
-    val map_error :
-      ('e error -> 'ee error) -> ('s, 'a, 'e) parser -> ('s, 'a, 'ee) parser
-val label : 'ee -> ('s, 'a, 'e) parser -> ('s, 'a, 'ee) parser
-val zero : ('s, 'a, 'e) parser
-val item : ('a, 'a, 'e) parser
+  ('s, 'a, 'e, 'ee) parser ->
+  ('a -> ('s, 'b, 'e, 'ee) parser) ->
+  ('s, 'b, 'e, 'ee) parser
+
+val return : 'a -> ('s, 'a, 'e, 'ee) parser
+val label : 'ee -> ('s, 'a, 'e, 'ee) parser -> ('s, 'a, 'e, 'ee) parser
+val zero : ('s, 'a, 'e, 'ee) parser
+val item : ('a, 'a, 'e, 'ee) parser
 
 val bind :
-  ('a -> ('s, 'b, 'e) parser) -> ('s, 'a, 'e) parser -> ('s, 'b, 'e) parser
+  ('a -> ('s, 'b, 'e, 'ee) parser) ->
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'b, 'e, 'ee) parser
 
+val ( <|> ) :
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'a, 'e, 'ee) parser
 
-val ( <|> ) : ('s, 'a, 'e) parser -> ('s, 'a, 'e) parser -> ('s, 'a, 'e) parser
-val alt : ('s, 'a, 'e) parser -> ('s, 'a, 'e) parser -> ('s, 'a, 'e) parser
-val choice : ('s, 'a, 'e) parser list -> ('s, 'a, 'e) parser
-val map : ('a -> 'b) -> ('s, 'a, 'e) parser -> ('s, 'b, 'e) parser
-val ( <$> ) : ('s, 'a, 'e) parser -> ('a -> 'b) -> ('s, 'b, 'e) parser
-val seq : ('s, 'a, 'e) parser -> ('s, 'b, 'e) parser -> ('s, 'a * 'b, 'e) parser
-val ( << ) : ('s, 'a, 'e) parser -> ('s, 'b, 'e) parser -> ('s, 'b, 'e) parser
+val alt :
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'a, 'e, 'ee) parser
+
+val choice : ('s, 'a, 'e, 'ee) parser list -> ('s, 'a, 'e, 'ee) parser
+val map : ('a -> 'b) -> ('s, 'a, 'e, 'ee) parser -> ('s, 'b, 'e, 'ee) parser
+val ( <$> ) : ('s, 'a, 'e, 'ee) parser -> ('a -> 'b) -> ('s, 'b, 'e, 'ee) parser
+
+val seq :
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'b, 'e, 'ee) parser ->
+  ('s, 'a * 'b, 'e, 'ee) parser
+
+val ( << ) :
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'b, 'e, 'ee) parser ->
+  ('s, 'b, 'e, 'ee) parser
 
 val keep_right :
-  ('s, 'a, 'e) parser -> ('s, 'b, 'e) parser -> ('s, 'b, 'e) parser
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'b, 'e, 'ee) parser ->
+  ('s, 'b, 'e, 'ee) parser
 
-val ( >> ) : ('s, 'a, 'e) parser -> ('s, 'b, 'e) parser -> ('s, 'a, 'e) parser
+val ( >> ) :
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'b, 'e, 'ee) parser ->
+  ('s, 'a, 'e, 'ee) parser
 
 val keep_left :
-  ('s, 'a, 'e) parser -> ('s, 'b, 'e) parser -> ('s, 'a, 'e) parser
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'b, 'e, 'ee) parser ->
+  ('s, 'a, 'e, 'ee) parser
 
 val between :
-  ('s, 'l, 'e) parser ->
-  ('s, 'r, 'e) parser ->
-  ('s, 'a, 'e) parser ->
-  ('s, 'a, 'e) parser
+  ('s, 'l, 'e, 'ee) parser ->
+  ('s, 'r, 'e, 'ee) parser ->
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'a, 'e, 'ee) parser
 
-val sat : ('a -> bool) -> ('a, 'a, 'e) parser
-val char : char -> (char, char, 'e) parser
-val digit : (char, char, 'e) parser
-val letter : (char, char, 'e) parser
-val lower : (char, char, 'e) parser
-val upper : (char, char, 'e) parser
-val word : (char, string, 'e) parser
-val word1 : (char, string, 'e) parser
-val alphanum : (char, char, 'e) parser
-val string : string -> (char, string, 'e) parser
-val many : ('s, 'a, 'e) parser -> ('s, 'a list, 'e) parser
-val many1 : ('s, 'a, 'e) parser -> ('s, 'a list, 'e) parser
-
+val sat : ('a -> bool) -> ('a, 'a, 'e, 'ee) parser
+val char : char -> (char, char, 'e, 'ee) parser
+val many : ('s, 'a, 'e, 'ee) parser -> ('s, 'a list, 'e, 'ee) parser
+val many1 : ('s, 'a, 'e, 'ee) parser -> ('s, 'a list, 'e, 'ee) parser
+val string : string -> (char, string, 'e, 'ee) parser
 val sepby :
-  ('s, 'a, 'e) parser -> ('s, 'b, 'e) parser -> ('s, 'a list, 'e) parser
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'b, 'e, 'ee) parser ->
+  ('s, 'a list, 'e, 'ee) parser
 
 val sepby1 :
-  ('s, 'a, 'e) parser -> ('s, 'b, 'e) parser -> ('s, 'a list, 'e) parser
+  ('s, 'a, 'e, 'ee) parser ->
+  ('s, 'b, 'e, 'ee) parser ->
+  ('s, 'a list, 'e, 'ee) parser
 
-val opt : ('s, 'a, 'e) parser -> ('s, 'a option, 'e) parser
-val count : int -> ('s, 'a, 'e) parser -> ('s, 'a list, 'e) parser
-val check : ('a -> bool) -> ('s, 'a, 'e) parser -> ('s, 'a, 'e) parser
+val opt : ('s, 'a, 'e, 'ee) parser -> ('s, 'a option, 'e, 'ee) parser
+val count : int -> ('s, 'a, 'e, 'ee) parser -> ('s, 'a list, 'e, 'ee) parser
+val check : ('a -> bool) -> ('s, 'a, 'e, 'ee) parser -> ('s, 'a, 'e, 'ee) parser
+val letter : (char, char, 'e, 'e) parser
+val digit : (char, char, 'e, 'e) parser
+val lower : (char, char, 'e, 'e) parser
+val upper : (char, char, 'e, 'e) parser
+
+val alphanum : (char, char, 'e, 'ee) parser
+val word : (char, string, 'e, 'e) parser
+
+val word1 : (char, string, 'e, 'e) parser
