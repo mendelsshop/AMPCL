f1de478de25a77d3d464fc7ff9a63e49452b1210
mendelsshop <mendelsshop@gmail.com>
2024-10-14 00:33:38 -0400

    Went a bit of the megaparsec way, some test still do not compile

--- lib/AMPCL.ml
+++ lib/AMPCL.ml
@@ -2,26 +2,46 @@
 type 'e error = { default : string; custom : 'e option }
 
 (*We have to error generics, to make map_error work properly*)
-type ('s, 'a, 'e, 'ee) parser =
-  's list -> 'e error -> ('a * 's list, 'ee error) result
+type ('s, 'a, 'e) parser =
+  | Parser of {
+      unParse :
+        'b 'ee.
+        's list ->
+        ('a -> 's list -> 's list * ('b, 'ee error) result) ->
+        ('e error -> 's list -> 's list * ('b, 'ee error) result) ->
+        's list * ('b, 'ee error) result;
+    }
 
 let ( & ) f g x = g (f x)
-let ( &. ) f g x y = g (f x y)
 
 (*let ( >> ) f g x = g (f x)*)
 let explode str = str |> String.to_seq |> List.of_seq
 let implode cs = cs |> List.to_seq |> String.of_seq
 
-let run p str =
-  (fun (parsed, str') ->
-    if List.length str' = 0 then Ok parsed
-    else Error { default = "eof"; custom = None })
-  |> Result.bind (p (str |> explode) { custom = None; default = "" })
+let run' (Parser { unParse }) str : char list * ('a, 'e error) result =
+  unParse (str |> explode) (fun a s -> (s, Ok a)) (fun e s -> (s, Error e))
 
-let return v input _ = Ok (v, input)
-let ( >>= ) p q input e = Result.bind (p input e) (fun (v, inp) -> q v inp e)
+let run p str = run' p str |> snd
+let return v = Parser { unParse = (fun s ok _ -> ok v s) }
+
+let ( >>= ) (Parser { unParse = unParseP }) q =
+  Parser
+    {
+      unParse =
+        (fun s ok err ->
+          let mok x s' =
+            let (Parser { unParse = unParseQ }) = q x in
+            unParseQ s' ok err
+          in
+          unParseP s mok err);
+    }
+
 let bind f p = p >>= f
-let zero _ _ = Error { default = "fail"; custom = None }
+
+let zero =
+  Parser
+    { unParse = (fun s _ err -> err { default = "fail"; custom = None } s) }
+
 let map f = bind (f & return)
 let ( <$> ) p f = map f p
 
@@ -37,29 +57,39 @@ let ( >> ) p q =
   q >>= fun _ -> return r
 
 let keep_left = ( >> )
+
 (*This does not work because, it takes a parser of 'e and returns a parser of 'ee*)
 (*But, we also take a new error ('ee) for the return parse (along with the input) which is fed into the orginal parser which expects 'e (this doesn't work for  base combinators, see item)*)
 (*Maybe just refactor parser definiton*)
-let map_error (f : 'e error -> 'ee error) p = p &. Result.map_error f
+let map_error f (Parser { unParse = p }) =
+  Parser
+    {
+      unParse =
+        (fun s ok err ->
+          let err' e s' = err (f e) s' in
+          let ok' e s' = ok e s' in
+          p s ok' err');
+    }
 
-let internal_label
-    (*:*)
-    (*    (string -> string) ->*)
-    (*    ('s, 'a, 'e, 'ee) parser ->*)
-    (*    ('s, 'a, 'ee, 'ee) parser*) e =
+let internal_label e =
   map_error (fun { custom; default } -> { custom; default = e default })
 
-let label (e : 'ee) =
+let label e =
   map_error (fun { custom = _; default } -> { custom = Some e; default })
 
-let ( <|> ) (p : ('s, 'a, 'e, 'ee) parser) (q : ('s, 'a, 'e, 'ee) parser)
-    input es  =
-  Result.fold
-    ~ok:(fun x -> Ok x)
-    ~error:(fun { default = e'; _ } ->
-      let x = q |> internal_label (fun e -> e' ^ " or " ^ e) in
-      x input es)
-    (p input es)
+let ( <|> ) (Parser { unParse = p }) q =
+  Parser
+    {
+      unParse =
+        (fun s ok err ->
+          let error { default = e; _ } s =
+            let (Parser { unParse = q' }) =
+              q |> internal_label (fun e' -> e ^ " or " ^ e')
+            in
+            q' s ok err
+          in
+          p s ok error);
+    }
 
 let alt = ( <|> )
 let between l r p = l << p >> r
@@ -68,29 +98,32 @@ let between l r p = l << p >> r
 let rec choice = function [] -> zero | fst :: rest -> fst <|> choice rest
 
 (*We cannot make this, be (..., 'e, 'e) parser, because then you cannot map error over it, but since we are plugging in the input error in*)
-let (item : ('a, 'a, 'e, 'ee) parser) =
- fun input error ->
-  match input with [] -> Error error | s :: rest -> Ok (s, rest)
+(*TODO: token parser might be better as it is more primitive / better errors*)
+let item =
+  Parser
+    {
+      unParse =
+        (fun input ok err ->
+          match input with
+          | [] -> err { default = "eof"; custom = None } input
+          | s :: rest -> ok s rest);
+    }
 
-let sat (p : 'a -> bool) : ('a, 'a, 'e, 'ee) parser =
-  item >>= fun x -> if p x then return x else zero
-
+let sat p = item >>= fun x -> if p x then return x else zero
 let char x = sat (fun y -> x == y) |> internal_label (fun _ -> String.make 1 x)
 
-let digit : (char, char, 'e, 'e) parser =
+let digit =
   sat (fun x -> '0' <= x && x <= '9') |> internal_label (fun _ -> "digit")
 
-let lower : (char, char, 'e, 'e) parser =
+let lower =
   sat (fun x -> 'a' <= x && x <= 'z')
   |> internal_label (fun _ -> "lower case letter")
 
-let upper : (char, char, 'e, 'e) parser =
+let upper =
   sat (fun x -> 'A' <= x && x <= 'Z')
   |> internal_label (fun _ -> "upper case letter")
 
-let letter : (char, char, 'e, 'e) parser =
-  upper <|> lower |> internal_label (fun _ -> "letter")
-
+let letter = upper <|> lower |> internal_label (fun _ -> "letter")
 let alphanum = letter <|> digit |> internal_label (fun _ -> " digit")
 
 let string str =
@@ -104,6 +137,7 @@ let string str =
   let exp_str : char list = List.of_seq (String.to_seq str) in
   string_i exp_str |> internal_label (fun _ -> str)
 
+(*TODO: better error*)
 let rec many parser =
   let neMany =
     parser >>= fun x ->
